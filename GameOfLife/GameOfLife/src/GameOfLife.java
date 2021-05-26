a64493ed56ccb86301535e4fdf5d53075ca1cf54
904154001 <904154001@L829WD150098.edu.cbe.ab.ca>
2021-05-26 13:33:08 -0600

    working

---
+++ GameOfLife/GameOfLife/src/GameOfLife.java
@@ -0,0 +1,35 @@
+
+public class GameOfLife {
+
+	int rows;
+	int columns;
+	int [][] twoDArray;
+	
+	public void GameOfLife(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		twoDArray = new boolean[rows][columns];
+	}
+	public int getRows() {
+		return rows;
+	}
+	public int getColumns() {
+		return columns;
+	}
+	
+	public boolean isAlive(int row, int column) {
+		boolean isAlive = false;
+		if (row < 0 || row > rows || column < 0 || column > columns) {
+			isAlive = false;
+		}
+		else {
+			isAlive = twoDArray[row][column];
+		}
+		return isAlive;
+	}
+	
+	public boolean setAlive(int row, int column, boolean life) {
+		twoDArray[row][column] = life;
+	}
+	
+}
